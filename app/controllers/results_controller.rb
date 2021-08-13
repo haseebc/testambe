e77bb24e16ba7d8f4ddbb643d6a75e5c7f504a3f
haseebc <haseebchaudhary@gmail.com>
2021-08-13 16:28:18 +0200

    stuff

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,31 @@
 class ResultsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[ index ]
+  skip_before_action :authenticate_user!, only: %i[ index show new create]
 
   def index
     @results = Result.all
   end
+
+  def show
+    @result = Result.find(params[:id])
+  end
+
+  def new
+    @result = Result.new
+  end
+
+  def create
+    @result = Result.new(clientref: "...", firstname: "...")
+
+    if @result.save
+      redirect_to @result
+    else
+      render :new
+    end
+  end
+
+  private
+  def result_params
+    params.require(:result).permit(:clientref, :firstname, :lastname, :dni, :clientemail, :result)
+  end
+
 end
